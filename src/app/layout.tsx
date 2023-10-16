af6880424f9a52a8a34420d8ad417e9fd7b4d9df
Carlos henrique <carloscardosojk.com>
2023-10-16 20:17:56 -0300

    language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import { cn } from "@/lib/utils";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={cn(inter.className, "bg-zinc-900 text-zinc-100")}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { Avatar } from "../ui/avatar";
+
+export const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 p-10">
+      <span>
+        Desenvolvido com por <a href=""></a>
+      </span>
+    </footer>
+  );
+};
