f1970e3a6e7e7a547c7a3150a301009ef87021f1
Carlos henrique <carloscardosojk.com>
2023-10-15 21:40:39 -0300

    Noticias

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Avatar } from "../ui/avatar";
+
+export const Header = () => {
+  return (
+    <header>
+      <Avatar></Avatar>
+      <strong>blob.g</strong>
+      <p>Noticias do mundo </p>
+    </header>
+  );
+};
